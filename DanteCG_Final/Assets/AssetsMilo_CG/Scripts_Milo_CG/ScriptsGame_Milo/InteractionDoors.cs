ea85bf9099ebd7ee942fb0a3112bbcb45821bdff
Juanca25-10 <juan_c.lopez_s@uao.edu.co>
2025-05-14 23:12:55 -0500

    jj
    jj

--- DanteCG_Final/Assets/AssetsMilo_CG/Scripts_Milo_CG/ScriptsGame_Milo/InteractionDoors.cs
+++ DanteCG_Final/Assets/AssetsMilo_CG/Scripts_Milo_CG/ScriptsGame_Milo/InteractionDoors.cs
@@ -4,8 +4,46 @@ using UnityEngine;
 
 public class InteractionDoors : MonoBehaviour, IInteractuable
 {
+    public bool doorOpen = true;
+    public float doorOpenAngle = 90f;
+    public float doorCloseAngle = 0f;
+    public float smooth = 2f;
+
+    public AudioClip openDoor;
+    public AudioClip closeDoor;
+
     public void ActivarObjeto()
     {
         Destroy(gameObject);
     }
+
+    void Update()
+    {
+        if (doorOpen)
+        {
+            Quaternion targetRotation = Quaternion.Euler(0, doorOpenAngle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, Time.deltaTime * smooth);
+        }
+        else
+        {
+            Quaternion targetRotation = Quaternion.Euler(0, doorCloseAngle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation, Time.deltaTime * smooth);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("TriggerDoor"))
+        {
+            AudioSource.PlayClipAtPoint(openDoor, transform.position, 1);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("TriggerDoor"))
+        {
+            AudioSource.PlayClipAtPoint(closeDoor, transform.position, 1);
+        }
+    }
 }
+
